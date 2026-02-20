b1b9deef0ae2ccb2abd498de3bd385019ac9023c
shivam <shvam12340987@gmail.com>
2026-02-20 23:40:42 +0530

    web search tool implemented , web search api is tavily

--- 04_tools/webSearch-integration.js
+++ 04_tools/webSearch-integration.js
@@ -40,14 +40,14 @@ const tools = [
     type: "function",
     function: {
       name: "web_search",
-      description: "Search web",
+      description: "search web for information",
       parameters: {
         // JSON Schema object
         type: "object",
         properties: {
           message: {
             type: "string",
-            description: "Message that user ask",
+            description: "user question",
           },
         },
         required: ["message"],
@@ -57,57 +57,48 @@ const tools = [
 ];
 
 async function main() {
-  const completion1 = await groq.chat.completions.create({
-    model: "llama-3.1-8b-instant",
-    tools: tools,
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful AI assistant.",
-      },
-      {
-        role: "user",
-        content: "when was i phone 17 was launched",
-      },
-    ],
-    tool_choice: "auto",
-  });
-
-  const toolsToCall = completion1.choices[0].message.tool_calls;
-  const message = completion1.choices[0].message;
-
-  if (!toolsToCall || !toolsToCall?.length) {
-    console.log("DIRECT ANSWER:= ", message);
-    return;
-  }
-
-  for (const tool of toolsToCall) {
-    const args = JSON.parse(tool.function.arguments);
-
-    const toolResult = await web_search(args.message);
-
-    const completion2 = await groq.chat.completions.create({
-      model: "llama-3.1-8b-instant",
-      temperature: 0.7,
-      messages: [
-        //   {
-        //     role: "system",
-        //     content:"give consize results"
-        //   },
-        {
-          role: "user",
-          content: "when was i phone 17 was launched",
-        },
-        message,
-        {
-          role: "tool",
-          tool_call_id: tool.id,
-          content: toolResult,
-        },
-      ],
+  let messages = [
+    {
+      role: "system",
+      content:
+        "You are a helpful AI assistant ",
+    },
+    {
+      role: "user",
+      content: "when was i phone 17 was launched",
+    },
+  ];
+  while (true) {
+    const completion1 = await groq.chat.completions.create({
+      model: "openai/gpt-oss-120b",
+      tools: tools,
+      messages: messages,
+      tool_choice: "auto",
     });
 
-    console.log("AFTER TOOL ANSWER:= ", completion2.choices[0].message);
+    const toolsToCall = completion1.choices[0].message.tool_calls;
+    const message = completion1.choices[0].message;
+    messages.push(message);
+
+    if (toolsToCall && toolsToCall?.length) {
+      for (const tool of toolsToCall) {
+        const args = JSON.parse(tool.function.arguments);
+
+        let toolResult;
+
+        if (tool.function.name == "web_search") {
+          toolResult = await web_search(args.message);
+          messages.push({
+            role: "tool",
+            tool_call_id: tool.id,
+            content: toolResult,
+          });
+        }
+      }
+    } else {
+      console.log(" ANSWER:= ", message);
+      break;
+    }
   }
 }
 
